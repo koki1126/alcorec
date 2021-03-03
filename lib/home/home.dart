896ba0896ac51d09c4b84e1541172e73bafac98d
yuto90 <solidusgako4087@gmail.com>
2021-03-03 15:41:44 +0900

    Recordへの画面遷移を実装

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -13,8 +13,58 @@ class HomePage extends StatelessWidget {
       create: (_) => HomeModel(),
       child: Consumer<HomeModel>(
         builder: (context, model, child) {
-          return Container(
-            child: Text('aaaaa'),
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('酒プラス'),
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerDocked,
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: Theme.of(context).accentColor,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Record(),
+                  ),
+                  //現状飛ばない
+                );
+              },
+              child: Icon(Icons.add),
+            ),
+            bottomNavigationBar: BottomAppBar(
+              color: Theme.of(context).primaryColor,
+              notchMargin: 6.0,
+              shape: AutomaticNotchedShape(
+                RoundedRectangleBorder(),
+                StadiumBorder(
+                  side: BorderSide(),
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(
+                        Icons.person_outline,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.info_outline,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ),
           );
         },
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,17 @@
+import 'package:alcorec/home/home.dart';
 import 'package:alcorec/record/record.dart';
 import 'package:flutter/material.dart';
-import 'home/home.dart';
-import 'footer.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(),
-        ),
-        body: HomePage(),
-        bottomNavigationBar: Footer(),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.brown.shade600,
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => Record(),
-              ),
-              //現状飛ばない
-            );
-          },
-          child: Icon(
-            Icons.add,
-            color: Colors.white70,
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
