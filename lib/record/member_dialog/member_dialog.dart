7e537331b347d262a84fd5d91129ff5ece0fc8d0
yuto90 <solidusgako4087@gmail.com>
2021-05-09 19:53:51 +0900

    Textfieldフォーカス時にキーボードでダイアログが隠れない

--- lib/record/member_dialog/member_dialog.dart
+++ lib/record/member_dialog/member_dialog.dart
@@ -17,72 +17,75 @@ class AddToMemberDialog extends StatelessWidget {
             content: Container(
               height: 500,
               width: 300,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Container(
-                    padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: TextFormField(
-                      keyboardType: TextInputType.text,
-                      controller: model.newMemberController,
-                      decoration: InputDecoration(
-                        hintText: 'Enter Name',
-                        icon: Icon(Icons.add_circle_outline),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                      child: TextFormField(
+                        keyboardType: TextInputType.text,
+                        controller: model.newMemberController,
+                        decoration: InputDecoration(
+                          hintText: 'Enter Name',
+                          icon: Icon(Icons.add_circle_outline),
+                        ),
                       ),
                     ),
-                  ),
-                  FlatButton(
-                    child: Text('登録'),
-                    color: Colors.blue,
-                    onPressed: () {
-                      model.newMemberController.text != ''
-                          ? model.memberInsert()
-                          : null; // 新規メンバーを追加
-                      //Navigator.pop(context);
-                    },
-                  ),
-                  Container(
-                    height: 20,
-                    width: double.infinity,
-                    child: Text(
-                      '▼ 登録済みのメンバー',
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Container(
-                    height: 360,
-                    child: FutureBuilder(
-                      future: model.getRegisteredMember(),
-                      builder: (BuildContext context, AsyncSnapshot snapshot) {
-                        if (snapshot.hasData) {
-                          model.checkboxList == null
-                              ? model.initValue(
-                                  snapshot.data,
-                                )
-                              : null;
-                          return ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: snapshot.data.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              return CheckboxListTile(
-                                value: model.checkboxList[index],
-                                title:
-                                    Text(snapshot.data[index]['member_name']),
-                                onChanged: (value) {
-                                  model.tapCheckbox(index, value);
-                                },
-                              );
-                            },
-                          );
-                        } else {
-                          return CircularProgressIndicator(); // データをロードできていなけれなぐるぐるを表示
-                        }
+                    FlatButton(
+                      child: Text('登録'),
+                      color: Colors.blue,
+                      onPressed: () {
+                        model.newMemberController.text != ''
+                            ? model.memberInsert()
+                            : Container(); // 新規メンバーを追加
+                        //Navigator.pop(context);
                       },
                     ),
-                  ),
-                ],
+                    Container(
+                      height: 20,
+                      width: double.infinity,
+                      child: Text(
+                        '▼ 登録済みのメンバー',
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      height: 360,
+                      child: FutureBuilder(
+                        future: model.getRegisteredMember(),
+                        builder:
+                            (BuildContext context, AsyncSnapshot snapshot) {
+                          if (snapshot.hasData) {
+                            model.checkboxList == null
+                                ? model.initValue(
+                                    snapshot.data,
+                                  )
+                                : null;
+                            return ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: snapshot.data.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                return CheckboxListTile(
+                                  value: model.checkboxList[index],
+                                  title:
+                                      Text(snapshot.data[index]['member_name']),
+                                  onChanged: (value) {
+                                    model.tapCheckbox(index, value);
+                                  },
+                                );
+                              },
+                            );
+                          } else {
+                            return CircularProgressIndicator(); // データをロードできていなけれなぐるぐるを表示
+                          }
+                        },
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             actions: <Widget>[
